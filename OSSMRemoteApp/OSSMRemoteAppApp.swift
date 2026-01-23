4982c105e25e417505ebb3304e08971ab70f14e1
ProtogenNotFound <bursa.dim7z@icloud.com>
2026-01-24 00:50:45 +0100

    Initial Commit

---
+++ OSSMRemoteApp/OSSMRemoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSSMRemoteAppApp.swift
+//  OSSMRemoteApp
+//
+//  Created by Bennet Kampe on 24/1/26.
+//
+
+import SwiftUI
+
+@main
+struct OSSMRemoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
